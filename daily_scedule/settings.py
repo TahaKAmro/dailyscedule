b9ad92142b448c10c729bf7076ddbf5b7989a7be
TahaKAmro <128462236+TahaKAmro@users.noreply.github.com>
2024-03-03 21:40:05 +0200

    Update settings.py

--- daily_scedule/settings.py
+++ daily_scedule/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4qd7o*$*6#u2lwwyvo^0k%^vgfq@b9+mm#dwd4o_m2fji$vxa6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
